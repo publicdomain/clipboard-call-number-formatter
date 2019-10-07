da0ea7ffcf9c082ba5dfb0bbb403dd368cd189bc
Victor L. Senior <publicdomainvic@gmail.com>
2019-10-07 06:42:36 -0400

    Add about dialog

--- ClipboardCallNumberFormatter/MainForm.cs
+++ ClipboardCallNumberFormatter/MainForm.cs
@@ -7,12 +7,13 @@ namespace ClipboardCallNumberFormatter
 {
     // Directives
     using System;
-    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Drawing;
+    using System.Reflection;
     using System.Runtime.InteropServices;
     using System.Text.RegularExpressions;
     using System.Windows.Forms;
+    using PublicDomain;
 
     /// <summary>
     /// Description of MainForm.
@@ -30,12 +31,35 @@ namespace ClipboardCallNumberFormatter
         private int callNumberCount = 0;
 
         /// <summary>
+        /// The assembly version.
+        /// </summary>
+        private Version assemblyVersion = Assembly.GetExecutingAssembly().GetName().Version;
+
+        /// <summary>
+        /// The semantic version.
+        /// </summary>
+        private string semanticVersion = string.Empty;
+
+        /// <summary>
+        /// The associated icon.
+        /// </summary>
+        private Icon associatedIcon = null;
+
+        /// <summary>
+        /// The friendly name of the program.
+        /// </summary>
+        private string friendlyName = "Clipboard Call Number Formatter";
+
+        /// <summary>
         /// Initializes a new instance of the <see cref="T:ClipboardCallNumberFormatter.MainForm"/> class.
         /// </summary>
         public MainForm()
         {
             // The InitializeComponent() call is required for Windows Forms designer support.
             this.InitializeComponent();
+
+            // Set semantic version
+            this.semanticVersion = this.assemblyVersion.Major + "." + this.assemblyVersion.Minor + "." + this.assemblyVersion.Build;
 
             // Add clipboard listener
             AddClipboardFormatListener(this.Handle);
@@ -182,7 +206,49 @@ namespace ClipboardCallNumberFormatter
         /// <param name="e">Event arguments.</param>
         private void OnAboutToolStripMenuItemClick(object sender, EventArgs e)
         {
-            // TODO Add code.
+            // Set license text
+            var licenseText = $"CC0 1.0 Universal (CC0 1.0) - Public Domain Dedication{Environment.NewLine}" +
+                $"https://creativecommons.org/publicdomain/zero/1.0/legalcode{Environment.NewLine}{Environment.NewLine}" +
+                $"Libraries and icons have separate licenses.{Environment.NewLine}{Environment.NewLine}" +
+                $"Document copy icon by Clker-Free-Vector-Images - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/document-button-duplicate-copy-35941/{Environment.NewLine}{Environment.NewLine}" +
+                $"Patreon icon used according to published brand guidelines{Environment.NewLine}" +
+                $"https://www.patreon.com/brand{Environment.NewLine}{Environment.NewLine}" +
+                $"GitHub mark icon used according to published logos and usage guidelines{Environment.NewLine}" +
+                $"https://github.com/logos{Environment.NewLine}{Environment.NewLine}" +
+                $"Reddit icon used according to published brand guidelines{Environment.NewLine}" +
+                $"https://www.reddit.com/wiki/licensing#wiki_using_the_reddit_brand{Environment.NewLine}{Environment.NewLine}" +
+                $"PublicDomain icon is based on the following source images:{Environment.NewLine}{Environment.NewLine}" +
+                $"Bitcoin by GDJ - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/vectors/bitcoin-digital-currency-4130319/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter P by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/p-glamour-gold-lights-2790632/{Environment.NewLine}{Environment.NewLine}" +
+                $"Letter D by ArtsyBee - Pixabay License{Environment.NewLine}" +
+                $"https://pixabay.com/illustrations/d-glamour-gold-lights-2790573/{Environment.NewLine}{Environment.NewLine}";
+
+            // Set about form
+            var aboutForm = new AboutForm(
+                $"About {this.friendlyName}",
+                $"{this.friendlyName} {this.semanticVersion}",
+                $"Made for: u/sindinha{Environment.NewLine}Reddit.com{Environment.NewLine}Week #41 @ October 2019",
+                licenseText,
+                this.Icon.ToBitmap());
+
+            // Check for an associated icon
+            if (this.associatedIcon == null)
+            {
+                // Set associated icon from exe file, once
+                this.associatedIcon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            }
+
+            // Set about form icon
+            aboutForm.Icon = this.associatedIcon;
+
+            // Match topmost
+            aboutForm.TopMost = this.TopMost;
+
+            // Show about form
+            aboutForm.ShowDialog();
         }
     }
 }
